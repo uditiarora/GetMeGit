d879819a81c28f4f3d38324eb6450eacf9c2ed59
Uditi <uditiarora@gmail.com>
2019-12-28 13:59:46 +0530

    adding new features'

--- index.js
+++ index.js
@@ -3,20 +3,34 @@ const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(telegramToken, {polling: true});
 
 var GhPolyglot = require("gh-polyglot");
-var async = require("async");
+const fetch = require("node-fetch");
 
-// telegram.on("text", (message) => {
-//   telegram.sendMessage(message.chat.id, "Hello world");
-// });
 
 var username = 'uditiarora'; //default username
 var user = new GhPolyglot(username + "/git-stats");
 var all_repo_stats = null;
+var user_data = null;
+var repo_data = null;
 
 bot.on("polling_error", (err) => console.log(err));
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
     const resp = "Welcome to GetMeGit bot. Type /user username to search for the user you are looking for. the default user is uditiarora";
+    fetch(`https://api.github.com/users/${username}`)
+            .then(response => {
+                if (response.status === 404) {
+                return setError({ active: true, type: 404 });
+                }
+                if (response.status === 403) {
+                return setError({ active: true, type: 403 });
+                }
+                return response.json();
+            })
+            .then(json => user_data = json)
+            .catch(error => {
+                setError({ active: true, type: 400 });
+                console.error('Error:', error);
+    });
     bot.sendMessage(chatId, resp);
 });
 
@@ -33,6 +47,26 @@ bot.onText(/\/user (.+)/, (msg, match) => {
             bot.sendMessage(chatId, resp);
         }
         else{
+            repo_data = null;
+            all_repo_stats = null;
+            user_data = null;
+            fetch(`https://api.github.com/users/${username}`)
+            .then(response => {
+                if (response.status === 404) {
+                return setError({ active: true, type: 404 });
+                }
+                if (response.status === 403) {
+                return setError({ active: true, type: 403 });
+                }
+                return response.json();
+            })
+            .then(json => user_data = json)
+            .catch(error => {
+                setError({ active: true, type: 400 });
+                console.error('Error:', error);
+            });
+
+
             all_repo_stats = stats;
             resp = "The user has a total of " + all_repo_stats.length +" repos.\n";
             resp += "Use the rest of the commands to view detailed info.";
@@ -42,21 +76,7 @@ bot.onText(/\/user (.+)/, (msg, match) => {
     
 });
 
-bot.onText(/\/rep_stats/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId,"Hold on! We are fetching the data.");
-    var resp = "These are the repository stats for the user. Languages in ascending order of usage are :\n";
-    user.repoStats(function (err, stats) {
-        if(err != null){
-            bot.sendMessage(chatId, "Sorry. Couldn't find anything :(");
-        }
-        else{
-            stats.forEach((element => resp += (element.label + "\n")));
-            bot.sendMessage(chatId,resp);
-        }
-    });
-    
-});
+
 
 bot.onText(/\/all_reps/, (msg) => {
     const chatId = msg.chat.id;
@@ -83,7 +103,7 @@ bot.onText(/\/all_reps/, (msg) => {
 
 });
 
-bot.onText(/\/user_stats/, (msg) => {
+bot.onText(/\/rep_stats/, (msg) => {
     const chatId = msg.chat.id;
     bot.sendMessage(chatId,"Hold on! We are fethcing the data.");
     var resp = "These are the user repo stats. Number of repositories in each language(descending order) : \n";
@@ -92,9 +112,167 @@ bot.onText(/\/user_stats/, (msg) => {
             bot.sendMessage(chatId, "Sorry. Couldn't find anything :(");
         }
         else{
-            stats.forEach((element => resp += (element.label + "\n")));
+            stats.forEach((element => resp += (element.label + ": " + element.value + "\n")));
             bot.sendMessage(chatId,resp);
         }
     });
     
+});
+
+bot.onText(/\/name/, (msg) => {
+    const chatId = msg.chat.id;
+    if(user_data != null){
+        bot.sendMessage(chatId, `User's name is ${user_data.name}.`);
+    }
+    else{
+        bot.sendMessage(chatId,"Couldn't find this user. Try some other user");
+    }    
+});
+
+bot.onText(/\/joining_date/, (msg) => {
+    const chatId = msg.chat.id;
+    if(user_data != null){
+        bot.sendMessage(chatId, `User's joining date was ${user_data.created_at}.`);
+    }
+    else{
+        bot.sendMessage(chatId,"Couldn't find this user. Try some other user");
+    }    
+});
+
+
+bot.onText(/\/followers/, (msg) => {
+    const chatId = msg.chat.id;
+    if(user_data != null){
+        bot.sendMessage(chatId, `User has ${user_data.followers} followers.`);
+    }
+    else{
+        bot.sendMessage(chatId,"Couldn't find this user. Try some other user.");
+    }    
+});
+
+bot.onText(/\/following/, (msg) => {
+    const chatId = msg.chat.id;
+    if(user_data != null){
+        bot.sendMessage(chatId, `User is following ${user_data.following} people.`);
+    }
+    else{
+        bot.sendMessage(chatId,"Couldn't find this user. Try some other user.");
+    }    
+});
+
+bot.onText(/\/location/, (msg) => {
+    const chatId = msg.chat.id;
+    if(user_data != null){
+        if(user_data.location != null){
+            bot.sendMessage(chatId,`User's location is ${user_data.location}.`);
+        }
+        else{
+            bot.sendMessage(chatId,"User has not provided any location.");
+        }
+    }
+    else{
+        bot.sendMessage(chatId,"Couldn't find this user. Try some other user.");
+    }    
+});
+bot.onText(/\/bio/, (msg) => {
+    const chatId = msg.chat.id;
+    if(user_data != null){
+        if(user_data.bio != null){
+            bot.sendMessage(chatId,`${user_data.bio}.`);
+        }
+        else{
+            bot.sendMessage(chatId,"User has not provided any bio.");
+        }
+    }
+    else{
+        bot.sendMessage(chatId,"Couldn't find this user. Try some other user.");
+    }    
+});
+
+bot.onText(/\/avatar/, (msg) => {
+    const chatId = msg.chat.id;
+    if(user_data != null){
+        if(user_data.avatar_url != null){
+            bot.sendMessage(chatId,`You can find user's avatar at ${user_data.avatar_url}`);
+        }
+        else{
+            bot.sendMessage(chatId,"User has not provided any avatar.");
+        }
+    }
+    else{
+        bot.sendMessage(chatId,"Couldn't find this user. Try some other user.");
+    }    
+});
+
+bot.onText(/\/company/, (msg) => {
+    const chatId = msg.chat.id;
+    if(user_data != null){
+        if(user_data.company != null){
+            bot.sendMessage(chatId,`User's company is ${user_data.company}.`);
+        }
+        else{
+            bot.sendMessage(chatId,"User has not provided any company.");
+        }
+    }
+    else{
+        bot.sendMessage(chatId,"Couldn't find this user. Try some other user.");
+    }    
+});
+
+function most_starred(){
+    const LIMIT = 10;
+        const sortProperty = 'stargazers_count';
+        const mostStarredRepos = repo_data
+            .filter(repo => !repo.fork)
+            .sort((a, b) => b[sortProperty] - a[sortProperty])
+            .slice(0, LIMIT);
+        var resp = "";
+        mostStarredRepos.forEach((repo => {
+            if(repo.name !== undefined){
+                resp += (repo.name + ": "+ repo[sortProperty]+"\n");
+            }
+        }));
+        return resp;
+}
+
+bot.onText(/\/most_starred/, (msg) => {
+    const chatId = msg.chat.id;
+    if(repo_data != null){
+        var resp = most_starred();
+        
+            resp = "Most starred repos with number of stars: \n" + resp;
+            bot.sendMessage(chatId,resp);
+        
+    }
+    else{
+        fetch(`https://api.github.com/users/${username}/repos?per_page=100`)
+            .then(response => {
+                if (response.status === 404) {
+                return setError({ active: true, type: 404 });
+                }
+                if (response.status === 403) {
+                return setError({ active: true, type: 403 });
+                }
+                return response.json();
+            })
+            .then(json => {
+                repo_data = json;
+                if(repo_data != null){
+                    var resp = most_starred();
+                   
+                        resp = "Most starred repos with number of stars: \n" + resp;
+                        bot.sendMessage(chatId,resp);
+                    
+                }
+                else{
+                    bot.sendMessage(chatId,"User has no repositories that are starred.");
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+        });
+
+        
+
+    }    
 });
--- stats.js
+++ stats.js
@@ -1,15 +1,45 @@
 // Dependencies
-var GhPolyglot = require("gh-polyglot");
+var username = "bchiang7";
+const fetch = require("node-fetch");
+var repoData;
+function most_starred(){
+  const LIMIT = 10;
+      const sortProperty = 'stargazers_count';
+      const mostStarredRepos = repo_data
+          .filter(repo => !repo.fork)
+          .sort((a, b) => b[sortProperty] - a[sortProperty])
+          .slice(0, LIMIT);
+      var resp = "";
+      mostStarredRepos.forEach((repo => {
+          if(repo.name !== undefined){
+              resp += (repo.name + ": "+ repo[sortProperty]+"\n");
+          }
+      }));
+      return resp;
+}
 
-// Stats about git-stats
-var user = new GhPolyglot("IonicaBizau/git-stats");
-
-
-user.repoStats(function (err, stats) {
-    console.log(err || stats);
-});
-
-user.userStats(function (err, stats) {
-    console.log(err || stats);
-});
-
+fetch(`https://api.github.com/users/${username}/repos?per_page=100`)
+            .then(response => {
+                if (response.status === 404) {
+                return setError({ active: true, type: 404 });
+                }
+                if (response.status === 403) {
+                return setError({ active: true, type: 403 });
+                }
+                return response.json();
+            })
+            .then(json => {
+                repo_data = json;
+                if(repo_data != null){
+                    var resp = most_starred();
+                   
+                        resp = "Most starred repos with number of stars: \n" + resp;
+                    console.log(resp);
+                }
+                else{
+                  console.log(resp);
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+        });
