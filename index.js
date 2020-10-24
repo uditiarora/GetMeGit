38dbf0a50f50831ad7056097ad3d69c41e8d9496
Uditi Arora <uditiarora@gmail.com>
2020-10-24 22:32:01 +0530

    updates

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const telegramToken = "your telegram bot token";
+const telegramToken = "1006092919:AAFSD6VDTAGWNOEvkblwkE-942cXhGWdAtA";
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(telegramToken, {polling: true});
 
 var GhPolyglot = require("gh-polyglot");
 const fetch = require("node-fetch");
 
-
-var username = 'uditiarora'; //default username
-var user = new GhPolyglot(username + "/git-stats");
+var user = new GhPolyglot('uditiarora' + "/git-stats");
 var all_repo_stats = null;
 var user_data = null;
 var repo_data = null;
@@ -15,10 +13,13 @@ var command_keyboard = [["/commands_list","/all_reps","/rep_stats"],
                         ["/name","/joining_date","/followers"],["/following","/location","/bio"],
                     ["/avatar","/company","/most_starred"]];
 
+var username_dict = {}
 bot.on("polling_error", (err) => console.log(err));
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
+    username_dict[chatId] = 'uditiarora';
     const resp = "Welcome to GetMeGit bot. Type /user username to search the user you are looking for. The default user is uditiarora.";
+    const username = username_dict[chatId];
     fetch(`https://api.github.com/users/${username}`)
             .then(response => {
                 if (response.status === 404) {
@@ -43,14 +44,15 @@ bot.onText(/\/start/, (msg) => {
 
 bot.onText(/\/user (.+)/, (msg, match) => {
     const chatId = msg.chat.id;
-    username = match[1];
+    username_dict[chatId] = match[1] === '' ? 'uditiarora' : match[1];
+    const username = username_dict[chatId];
     var resp = "Hold on while we fetch the user info.";
     bot.sendMessage(chatId, resp);
     user = new GhPolyglot(username + "/git-stats");
     user.getAllRepos(function(err,stats) {
         if(err != null){
             resp = "User not found. Try another user.";
-            username = "uditiarora";
+            username_dict[chatId] = "uditiarora";
             bot.sendMessage(chatId, resp);
         }
         else{
@@ -87,6 +89,7 @@ bot.onText(/\/user (.+)/, (msg, match) => {
 
 bot.onText(/\/all_reps/, (msg) => {
     const chatId = msg.chat.id;
+    const username = username_dict[chatId];
     bot.sendMessage(chatId,"Hold on while we fetch the repository names for you.");
     var resp = "The reps are :\n";
     if(all_repo_stats != null){
@@ -112,7 +115,7 @@ bot.onText(/\/all_reps/, (msg) => {
 
 bot.onText(/\/rep_stats/, (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId,"Hold on! We are fethcing the data.");
+    bot.sendMessage(chatId,"Hold on! We are fetching the data.");
     var resp = "These are the user repo stats. Number of repositories in each language(descending order) : \n";
     user.userStats(function (err, stats) {
         if(err != null){
@@ -139,7 +142,7 @@ bot.onText(/\/name/, (msg) => {
 bot.onText(/\/joining_date/, (msg) => {
     const chatId = msg.chat.id;
     if(user_data != null){
-        bot.sendMessage(chatId, `User's joining date was ${user_data.created_at.substring(0,10)}.`);
+        bot.sendMessage(chatId, `User's joining date is ${user_data.created_at.substring(0,10)}.`);
     }
     else{
         bot.sendMessage(chatId,"Couldn't find this user. Try some other user");
@@ -244,6 +247,7 @@ function most_starred(){
 
 bot.onText(/\/most_starred/, (msg) => {
     const chatId = msg.chat.id;
+    const username = username_dict[chatId];
     if(repo_data != null){
         var resp = most_starred();
         if(resp.localeCompare("") === 0){
